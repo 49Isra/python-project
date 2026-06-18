09ef784c42bee9b8733d7cce57923cd1f1958103
Isra Tinmekar <isratinmekar@gmail.com>
2026-06-18 13:13:49 +0530

    Added the number guessing game

---
+++ number guess game/script.js
@@ -0,0 +1,23 @@
+let randomNumber = Math.floor(Math.random() * 10) + 1;
+
+function checkGuess() {
+    let guess = document.getElementById("guess").value;
+    let message = document.getElementById("message");
+
+    if (guess === "") {
+        message.textContent = "Please enter a number!";
+        return;
+    }
+
+    guess = Number(guess);
+
+    if (guess > randomNumber) {
+        message.textContent = "Too High!";
+    }
+    else if (guess < randomNumber) {
+        message.textContent = "Too Low!";
+    }
+    else {
+        message.textContent = "🎉 Congratulations! You got it!";
+    }
+}
